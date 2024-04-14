a9f20f6ee843f346c835384b70e7a3824f7a3dc4
Ascurius <coding@ascurius.com>
2024-04-14 02:09:44 +0200

    Added first working prototype of GROUP BY operator

--- code/operators.py
+++ code/operators.py
@@ -1,6 +1,11 @@
 from Compiler.types import sint
 from Compiler.library import for_range_opt, if_
 
+
+########################
+#### Secure operators
+########################
+
 def inner_join_nested_loop(left, right, key=0):
     """
     The key=0 represents the zeroth column which usualy is the id.
@@ -26,3 +31,47 @@ def inner_join_nested_loop(left, right, key=0):
 
 def order_by(table, key):
     table.sort((key,))
+
+def groub_by(array):
+    result = MultiArray([arr_test.length, 2], sint)
+    count = sint(1)
+    current_element = sint(0)
+    @for_range_opt(array.length)
+    def _(i):
+        dbit = (array[i] != current_element).if_else(1,0).reveal()
+        @if_e(dbit)
+        def _():
+            @if_((current_element != sint(0)).if_else(1,0).reveal())
+            def _():
+                result[i][0] = current_element
+                result[i][1] = count
+            current_element.update(array[i])
+            count.update(sint(1))
+        @else_
+        def _():
+            count.update(count + sint(1))
+    @if_((current_element != sint(0)).if_else(1,0).reveal())
+    def _():
+        result[-1][0] = current_element
+        result[-1][1] = count
+    return result
+
+########################
+#### Plaintext operators
+########################
+
+def group_by(arr):
+    result = []
+    count = 1
+    current_element = None
+    for i in range(len(arr)):
+        if arr[i] != current_element:
+            if current_element is not None:
+                result.append([current_element, count])
+            current_element = arr[i]
+            count = 1
+        else:
+            count += 1
+    if current_element is not None:
+        result.append([current_element, count])
+    return result
