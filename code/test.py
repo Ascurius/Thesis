45a8a0b0e0e100f99aea68aa0876ba432b804886
Ascurius <coding@ascurius.com>
2024-05-02 02:01:07 +0200

    added script and dummy data to generate duckdb database

--- code/test.py
+++ code/test.py
@@ -1,15 +1,4 @@
-import numpy as np
-import pandas as pd
-import time
+import duckdb
 
-player = 0
-filename = "/home/martin/Masterarbeit/MP-SPDZ_latest/Player-Data/Input-P{}-0".format(player)
-
-num_rows = 100000000
-start_time = time.time()
-m = np.fromfile(filename, sep=" ", dtype=int, count=num_rows*13)
-m = m.reshape((num_rows,13))
-end_time = time.time()
-execution_time = end_time - start_time
-print(execution_time)
+cursor = duckdb.connect()
 
