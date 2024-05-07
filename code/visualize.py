f1d99e96b67fcd77688d368c852c7a45392d6420
Ascurius <coding@ascurius.com>
2024-05-07 17:57:15 +0200

    restructured script

--- code/visualize.py
+++ code/visualize.py
@@ -1,73 +1,66 @@
+import os
+import csv
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def read_plain(filename):
+    rows = []
+    query_times = []
+
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row
+        for row in reader:
+            rows.append(int(row[0]))
+            query_times.append(float(row[1]))
+
+    return rows, query_times
+
+pwd = os.getcwd()
+query = "comorbidity"
+
+# Data for plain query
+filename_plain = f"{pwd}/measurements/"
+plain_rows, plain_times = read_plain(filename_plain)
+
+# Data for secure query
+secure_rows = [1000, 10000, 100000]
+secure_times = [0.04807, 1.36505, 17.435]
+
+# Perform linear regression
+slope, intercept, _, _, _ = linregress(np.log(secure_rows), np.log(secure_times))
+
+# Extrapolate the next data point (1,000,000 rows)
+next_row = 1000000
+next_time = np.exp(intercept + slope * np.log(next_row))
+secure_rows.append(next_row)
+secure_times.append(next_time)
+
+# Data for SQL query
+sql_rows = [1000, 10000, 100000, 1000000]
+sql_times = [0.0055, 0.0058, 0.0110, 0.0144]
+
+plt.figure(figsize=(10, 6))
+
+# Plotting plain query
+plt.plot(plain_rows, plain_times, marker='o', label='Python')
+
+# Plotting secure query
+plt.plot(secure_rows[:3], secure_times[:3], marker='o', linestyle='-', label='MP-SPDZ')
+plt.plot(secure_rows[2:], secure_times[2:], marker='o', linestyle=':', color='orange')
 
 
-def query_comparison():
-    # Data for plain query
-    plain_rows = [1000, 10000, 100000, 1000000]
-    plain_times = [0.000152, 0.001439, 0.015146, 0.223941]
+# Plotting SQL query
+plt.plot(sql_rows, sql_times, marker='o', label='DuckDB')
 
-    # Data for secure query
-    secure_rows = [1000, 10000, 100000]
-    secure_times = [0.04807, 1.36505, 17.435]
-
-    # Data for SQL query
-    sql_rows = [1000, 10000, 100000, 1000000]
-    sql_times = [0.0055, 0.0058, 0.0110, 0.0144]
-
-    plt.figure(figsize=(10, 6))
-
-    # Plotting plain query
-    plt.plot(plain_rows, plain_times, marker='o', label='Plain')
-
-    # Plotting secure query
-    plt.plot(secure_rows, secure_times, marker='o', label='Secure')
-
-    # Plotting SQL query
-    plt.plot(sql_rows, sql_times, marker='o', label='SQL')
-
-    plt.title('Query Time Comparison')
-    plt.xlabel('Number of Rows')
-    plt.ylabel('Query Time (s)')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.grid(True)
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig('query_time_comparison.png')
-
-def secure_metrics():
-    # Data for secure query resources
-    rows = [1000, 10000, 100000]
-    data_size = [2.58, 65.40, 762.34]  # Data size in MB
-    rounds = [437999, 12719671, 150003463]  # Rounds
-    time = [48.07, 1365.05, 17435]  # Time in seconds
-
-    plt.figure(figsize=(10, 8))
-
-    # Plotting data size
-    plt.subplot(3, 1, 1)
-    plt.plot(rows, data_size, marker='o', color='blue')
-    plt.title('Secure Query Resources')
-    plt.ylabel('Data Size (MB)')
-    plt.xscale('log')
-    plt.grid(True)
-
-    # Plotting rounds
-    plt.subplot(3, 1, 2)
-    plt.plot(rows, rounds, marker='o', color='orange')
-    plt.ylabel('Rounds')
-    plt.xscale('log')
-    plt.grid(True)
-
-    # Plotting time
-    plt.subplot(3, 1, 3)
-    plt.plot(rows, time, marker='o', color='green')
-    plt.xlabel('Number of Rows')
-    plt.ylabel('Time (seconds)')
-    plt.xscale('log')
-    plt.grid(True)
-
-    plt.tight_layout()
-    plt.savefig('secure_metrics.png')
-
-secure_metrics()
+plt.title('Query Time Comparison')
+plt.xlabel('Number of Rows')
+plt.ylabel('Query Time (s)')
+plt.xscale('log')
+plt.yscale('log')
+plt.grid(True)
+plt.legend()
+plt.legend(['Measured Data', 'Extrapolated Data'], loc='upper left')
+plt.tight_layout()
+plt.savefig('query_time_comparison.png')
