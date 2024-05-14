0e2cbf6fcc022d202ad30c56b3678b00da8c4ae0
Ascurius <coding@ascurius.com>
2024-05-14 19:10:45 +0200

    tested computation of hashes from sint

--- code/test.py
+++ code/test.py
@@ -1,8 +1,4 @@
 from circuit import sha3_256
 
-max_rows = 10
-a = sint.Matrix(max_rows, 13)
-a.input_from(0)
-
-for i in range(a.shape[0]):
-    print_ln("%s", sha3_256(a[i]).reveal())
+a = sint(1)
+h = sha3_256(a)
