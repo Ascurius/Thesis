0b57f378406806119ce7fceac39925516f0cee88
Ascurius <coding@ascurius.com>
2024-05-14 19:11:45 +0200

    added where clause to query

--- code/queries/secure/aspirin_count.py
+++ code/queries/secure/aspirin_count.py
@@ -5,9 +5,8 @@ def print_matrix(matrix: sint.Matrix):
 
 def print_matches(matrix: sint.Matrix, match_keys: list):
     for row in range(matrix.shape[0]):
-        for column in match_keys:
-            if matrix[row][column].reveal() == 1:
-                println(matrix[row].reveal())
+        if all(matrix[row][col] for col in match_keys):
+            print_ln("%s", matrix[row].reveal())
 
 def select_distinct(matrix: sint.Matrix, column: int) -> sint.Matrix:
     matrix.sort((column, ))
@@ -54,6 +53,10 @@ def inner_join_nested_loop(
             current_idx.update(current_idx + regint(1))
     return result
 
+def where(matrix: sint.Matrix, key: int, value: int) -> sint.Matrix:
+    pass
+
+
 max_rows = 10
 a = sint.Matrix(max_rows, 13)
 a.input_from(0)
@@ -65,6 +68,25 @@ j = inner_join_nested_loop(a, b, 1, 1)
 
 s = select_distinct(j, 1)
 
+result = sint.Matrix(
+    rows=a.shape[0],
+    columns=a.shape[1] + 1
+)
+@for_range_opt(a.shape[0])
+def _(i):
+    result[i].assign_vector(a[i])
 
-print_matrix(s)
-# print_matches(s, [-1, ])
+@for_range_opt(a.shape[0])
+def _(i):
+    dbit_1 = (a[i][8] == 414).if_else(sint(1), sint(0))
+    dbit_2 = (b[i][4] == 0).if_else(sint(1), sint(0))
+    dbit_3 = (a[i][2] <= b[i][2]).if_else(sint(1), sint(0))
+    dbit_final = dbit_1.bit_and(dbit_2.bit_and(dbit_3))
+    result[i][a.shape[1]] = dbit_final
+
+# @for_range_opt(s.shape[0])
+# def _(row):
+#     dbit = ((s[row][s.shape[1]-1].reveal() == sint(1)) and (s[row][s.shape[1]-2].reveal() == sint(1))).if_else(0,1)
+#     @if_(dbit.reveal())
+#     def _():
+#         print_ln("%s", s[row].reveal())
