e738dddfaf6b40fa075f26d1e78e04d02830b380
Ascurius <coding@ascurius.com>
2024-05-21 13:38:20 +0200

    fixed an error in DISTINCT operator in aspirin_count query

--- code/queries/plain/aspirin_count.py
+++ code/queries/plain/aspirin_count.py
@@ -2,47 +2,73 @@ from pprint import pprint
 from collections import defaultdict
 import numpy as np
 
-def preprocess(filename, num_rows):
-    m = np.fromfile(filename, sep=" ", dtype=int, count=num_rows*13)
-    m = m.reshape((num_rows,13))
-    return m
+def preprocess(filename):
+    with open(filename, 'r') as file:
+        list_of_lists = []
+        for line in file:
+            elements = list(map(int, line.split()))
+            list_of_lists.append(elements)
+    return list_of_lists
 
-def select_distinct(matrix: np.ndarray, column: int) -> np.ndarray:
-    # unique_values = np.unique(matrix[:, column], axis=0)
-    unique_values = []
-    for element in matrix[:, column]:
-        if element not in unique_values:
-            unique_values.append(element)
-    return unique_values
+def select_distinct(matrix: list, column: int) -> list:
+    result = []
+    prev_value = None
+    for i in range(len(matrix)):
+        if (matrix[i][column] != prev_value) & (matrix[i][-1] == matrix[i][-2] == matrix[i][-3] == matrix[i][13] == 1):
+            result.append(matrix[i] + [1])
+            prev_value = matrix[i][column]
+        else:
+            result.append(matrix[i] + [0])
+    return result
 
 def nested_loop_join(left, right, left_key, right_key):
     result = []
     for l_row in left:
         for r_row in right:
             if r_row[right_key] == l_row[left_key]:
-                result.append(
-                    np.concatenate((l_row, r_row))
-                )
-    return np.array(result)
+                result.append(l_row + r_row + [1])
+            else:
+                result.append(l_row + r_row + [0])
+    return result
 
 def where(matrix, key, value):
-    return matrix[matrix[:, key] == value]
+    for i in range(len(matrix)):
+        if matrix[i][key] == value:
+            matrix[i].append(1)
+        else:
+            matrix[i].append(0)
+    return matrix
 
-a = preprocess("/home/mpretz/Thesis/MP-SPDZ/Player-Data/Input-P0-0", 50)
-b = preprocess("/home/mpretz/Thesis/MP-SPDZ/Player-Data/Input-P1-0", 50)
+def where_less_then(matrix, col1, col2):
+    for i in range(len(matrix)):
+        if matrix[i][col1] <= matrix[i][col2]:
+            matrix[i].append(1)
+        else:
+            matrix[i].append(0)
+    return matrix
+
+a = preprocess("/home/mpretz/Thesis/MP-SPDZ/Player-Data/Input-P0-0")
+b = preprocess("/home/mpretz/Thesis/MP-SPDZ/Player-Data/Input-P1-0")
 
 aw = where(a, 8, 414)
 bw = where(b, 4, 0)
 
 j = nested_loop_join(aw, bw, 1, 1)
 
-# result = []
-# for row in j:
-#     print(row[:8])
-#     if row[2] <= row[len(bw[0])+2]:
-#         result.append(np.concatenate((row[0], row[1])))
-# result = np.array(result)
+m = where_less_then(j, 2, len(aw[0])+2)
 
-s = select_distinct(j, 0)
-c = len(j)
-print(c)
+
+
+# print(s)
+# m = []
+# for row in lt:
+#     if row[-1] == row[-2] == row[-3] == row[13] == 1:
+#         m.append(row)
+        
+s = select_distinct(m, 0)
+c = 0
+for row in s:
+    if row[-1] == 1:
+        print(row[0])
+        c += 1
+print("Total count: {}".format(c))
--- code/queries/secure/aspirin_count.py
+++ code/queries/secure/aspirin_count.py
@@ -9,21 +9,6 @@ def print_matches(matrix, match_key):
     for i in range(matrix.shape[0]):
         print_ln_if(matrix[i][match_key].reveal(), "%s", matrix[i].reveal())
 
-def select_relevant(matrix):
-    result = sint.Matrix(
-        rows=matrix.shape[0],
-        columns=matrix.shape[1]
-    )
-    idx = regint(0)
-    @for_range_opt(matrix.shape[0])
-    def _(i):
-        dbit = (matrix[i][-1] & matrix[i][-2]).if_else(1,0)
-        @if_(dbit.reveal())
-        def _():
-            result[idx].assign_vector(matrix[i])
-            idx.update(idx + 1)
-    return result
-
 def count_relevant(matrix, relevancy_key):
     idx = regint(0)
     @for_range_opt(matrix.shape[0])
@@ -35,38 +20,27 @@ def count_relevant(matrix, relevancy_key):
     return idx
 
 
-def select_distinct(matrix: sint.Matrix, column: int) -> sint.Matrix:
-    # matrix.sort((column,))
+def select_distinct(matrix: sint.Matrix, key: int) -> sint.Matrix:
+    matrix.sort((key,))
     result = sint.Matrix(
         rows=matrix.shape[0],
         columns=matrix.shape[1] + 1
     )
+
+    prev_value = sint(-1)
     @for_range_opt(matrix.shape[0])
     def _(i):
         result[i].assign_vector(matrix[i])
-
-    rel = matrix.shape[1] # index of the relevancy column
-    @for_range_opt(matrix.shape[0]-1)
-    def _(i):
-        dbit_join = (matrix[i][-1] == 1).if_else(1,0)
-        dbit_wa = (matrix[i][a.shape[1]] == 1).if_else(1,0)
-        dbit_wb = (matrix[i][-2] == 1).if_else(1,0)
-        dbit_rel = (matrix[i][column] == matrix[i+1][column]).if_else(0,1)
-        result[i][rel] = (dbit_join & dbit_wa & dbit_wb & dbit_rel).if_else(1,0)
-
-        # print_ln("i: %s, i+1: %s", matrix[i][column].reveal(), matrix[i+1][column].reveal())
-        # print_ln("JOIN: %s", matrix[i][-1].reveal())
-        # print_ln("WHERE (for table a): %s", matrix[i][a.shape[1]].reveal())
-        # print_ln("WHERE (for table b): %s", matrix[i][-2].reveal())
-        # print_ln("i: %s, i+1: %s, dbit: %s", matrix[i][column].reveal(), matrix[i+1][column].reveal(), result[i][rel].reveal())
-        # result[i][rel] = dbit
-        # @if_(dbit.reveal())
-        # def _():
-        #     print_ln("i: %s, i+1: %s", matrix[i][column].reveal(), matrix[i+1][column].reveal())
-        #     print_ln("JOIN: %s", matrix[i][-1].reveal())
-        #     print_ln("WHERE (for table b): %s", matrix[i][a.shape[1]-1].reveal())
-        #     print_ln("WHERE (for table a): %s", matrix[i][-2].reveal())
-    result[-1][rel] = sint(1)
+        dbit = (
+            (matrix[i][key] != prev_value) &
+            (matrix[i][-1] == 1) &
+            (matrix[i][-2] == 1) &
+            (matrix[i][-3] == 1) &
+            (matrix[i][13] == 1)
+        ).if_else(sint(1),sint(0))
+        result[i][-1] = dbit
+        new_value = (dbit == 1).if_else(matrix[i][key], prev_value)
+        prev_value.update(new_value)
     return result
 
 def inner_join_nested_loop(
@@ -107,6 +81,18 @@ def where(matrix, key, value):
         result[i][-1] = (matrix[i][key] == value).if_else(1,0)
     return result
 
+def where_less_then(matrix, col_1, col_2):
+    result = sint.Matrix(
+        rows=matrix.shape[0],
+        columns=matrix.shape[1] + 1
+    )
+    @for_range_opt(matrix.shape[0])
+    def _(i):
+        result[i].assign_vector(matrix[i])
+        result[i][-1] = (
+            matrix[i][col_1] <= matrix[i][col_2]
+        ).if_else(1,0)
+    return result
 
 max_rows = 50
 a = sint.Matrix(max_rows, 13)
@@ -117,21 +103,18 @@ b.input_from(1)
 aw = where(a, 8, 414)
 bw = where(b, 4, 0)
 
-matrix = inner_join_nested_loop(a, b, 1, 1)
+join = inner_join_nested_loop(aw, bw, 1, 1)
 
-# matrix = select_distinct(matrix, 0)
+wlt = where_less_then(join, 2, aw.shape[1]+2)
+
+select = select_distinct(wlt, 0)
 
 idx = regint(0)
-@for_range_opt(matrix.shape[0])
+@for_range_opt(select.shape[0])
 def _(i):
-    join = (matrix[i][-1] == 1).if_else(1,0)
-    where_a = (matrix[i][aw.shape[1]] == 1).if_else(1,0)
-    where_b = (matrix[i][-2] == 1).if_else(1,0)
-    # dbit = (join & where_a & where_b).if_else(1,0)
-    dbit = (matrix[i][-1] == 1).if_else(1,0)
-    @if_(dbit.reveal())
+    dbit_5 = (select[i][-1] == 1).if_else(1,0) # select distinct
+    @if_(dbit_5.reveal())
     def _():
-        # print_ln("%s %s %s", join.reveal(), where_a.reveal(), where_b.reveal())
-        print_ln("%s", matrix[i].reveal())
+        print_ln("%s", select[i].reveal())
         idx.update(idx + 1)
 print_ln("%s", idx)
