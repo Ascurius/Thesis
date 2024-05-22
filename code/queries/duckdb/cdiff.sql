28e2916a4bd9fc0bf7418c8b7ea8f43297de5e53
Ascurius <coding@ascurius.com>
2024-05-22 12:36:32 +0200

    added working duckdb representation of cdiff query

--- code/queries/duckdb/cdiff.sql
+++ code/queries/duckdb/cdiff.sql
@@ -1,9 +1,9 @@
-WITH diags (field0, field10, r) AS (
-    SELECT field0,field10, row_number() OVER (PARTITION BY field0 ORDER BY field10) AS r
+WITH diags (field0, field2, r) AS (
+    SELECT field0,field2, row_number() OVER (PARTITION BY field0 ORDER BY field2) AS r
     FROM table1
     WHERE field8 == 8
 )
 SELECT DISTINCT a.field0
     FROM diags a JOIN diags b ON a.field0 == b.field0
-    WHERE datediff('day', a.field10, b.field10) >= 15 
-    AND datediff('day', a.field10, b.field10) <= 56 AND a.r+1 = b.r
+    WHERE abs(a.field2 - b.field2) >= 15 
+    AND abs(a.field2 - b.field2) <= 56 AND a.r+1 = b.r
