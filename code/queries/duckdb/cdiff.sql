060eb646083f7abf4eed452dcdfdede1a1fc46a2
Ascurius <coding@ascurius.com>
2024-05-23 14:04:39 +0200

    fixed an error on the cdiff query

--- code/queries/duckdb/cdiff.sql
+++ code/queries/duckdb/cdiff.sql
@@ -1,9 +1,9 @@
-WITH diags (field0, field2, r) AS (
-    SELECT field0,field2, row_number() OVER (PARTITION BY field0 ORDER BY field2) AS r
+WITH diags (field1, field2, r) AS (
+    SELECT field1,field2, row_number() OVER (PARTITION BY field1 ORDER BY field2) AS r
     FROM table1
     WHERE field8 == 8
 )
-SELECT DISTINCT a.field0
-    FROM diags a JOIN diags b ON a.field0 == b.field0
+SELECT DISTINCT a.field1
+    FROM diags a JOIN diags b ON a.field1 == b.field1
     WHERE abs(a.field2 - b.field2) >= 15 
     AND abs(a.field2 - b.field2) <= 56 AND a.r+1 = b.r
