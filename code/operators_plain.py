8cfc8d174a2e8ccfacd61c457da9040d4f44f2d3
Ascurius <coding@ascurius.com>
2024-05-23 14:06:22 +0200

    added cdiff operators

--- code/operators_plain.py
+++ code/operators_plain.py
@@ -1,4 +1,5 @@
-from typing import List
+from collections import defaultdict
+from typing import Callable, List, Tuple
 import numpy as np
 from itertools import groupby
 
@@ -14,15 +15,34 @@ def preprocess(filename: str):
             list_of_lists.append(elements)
     return list_of_lists
 
-def select_columns(matrix: np.ndarray, columns: list) -> np.ndarray:
+def select(matrix: List[List[int]], columns: List[int]) -> List[List[int]]:
     columns.sort()
-    return matrix[:, columns]
+    return [[row[i] for i in columns] for row in matrix]
 
-def group_by_count(matrix: np.ndarray, key: int) -> np.ndarray:
-    column = sorted(matrix[:, key])
-    return np.array(
-        ([(k, len(list(g))) for k, g in groupby(column)])
-    )
+def group_by(matrix: List[List[int]], key: int) -> List[List[int]]:
+    matrix.sort(key=lambda row: row[key])
+    count = 0
+    current_element = -1
+
+    for i in range(len(matrix)-1):
+        matrix[i].extend([0,0])
+        if matrix[i][key] == matrix[i+1][key]:
+            matrix[i][-2] = 0
+        else:
+            matrix[i][-2] = 1
+        if matrix[i][key] == current_element:
+            count = count + 1
+            current_element = current_element
+        else:
+            count = 1
+            current_element = matrix[i][key]
+        matrix[i][-1] = count
+    matrix[-1][-2] = 1
+    if matrix[-1][key] == current_element:
+        matrix[-1][-1] = count + 1
+    else:
+        matrix[-1][-1] = 1
+    return matrix
 
 def order_by(matrix, keys, reversed=False):
     sorted_matrix = sorted(matrix, key=lambda x: (x[keys[1]], x[keys[0]]), reverse=reversed)
@@ -39,11 +59,15 @@ def hash_join(left, right, left_key, right_key):
     # join phase
     return [(s, r) for r in right for s in h[r[right_key]]]
 
-def select_distinct(matrix: List[List[int]], column: int) -> List[List[int]]:
+def select_distinct(
+        matrix: List[List[int]], 
+        column: int,
+        condition: Callable[[List[int]], bool] = lambda row: True
+    ) -> List[List[int]]:
     result = []
     prev_value = None
     for i in range(len(matrix)):
-        if (matrix[i][column] != prev_value) & (matrix[i][-1] == matrix[i][-2] == matrix[i][-3] == matrix[i][13] == 1):
+        if (matrix[i][column] != prev_value) and condition(matrix[i]):
             result.append(matrix[i] + [1])
             prev_value = matrix[i][column]
         else:
@@ -54,24 +78,27 @@ def nested_loop_join(
         left: List[List[int]],
         right: List[List[int]], 
         left_key: int, 
-        right_key: int
-    ) -> List[List[int]]:
+        right_key: int,
+        condition: Callable[[List[int], List[int]], bool] = lambda left, right: True
+    ) -> Tuple[List[List[int]], int]:
+    relevancy_column = len(left[0]) + len(right[0])
     result = []
     for l_row in left:
         for r_row in right:
-            if r_row[right_key] == l_row[left_key]:
+            if (r_row[right_key] == l_row[left_key]) and condition(l_row, r_row):
                 result.append(l_row + r_row + [1])
             else:
                 result.append(l_row + r_row + [0])
-    return result
+    return result, relevancy_column
 
-def where(matrix: List[List[int]], key: int, value: int) -> List[List[int]]:
+def where(matrix: List[List[int]], key: int, value: int) -> Tuple[List[List[int]], int]:
+    relevancy_column = len(matrix[0])
     for i in range(len(matrix)):
         if matrix[i][key] == value:
             matrix[i].append(1)
         else:
             matrix[i].append(0)
-    return matrix
+    return matrix, relevancy_column
 
 def where_less_then(matrix: List[List[int]], col1: int, col2: int) -> List[List[int]]:
     for i in range(len(matrix)):
@@ -80,3 +107,33 @@ def where_less_then(matrix: List[List[int]], col1: int, col2: int) -> List[List[
         else:
             matrix[i].append(0)
     return matrix
+
+def row_number_over_partition_by(
+        matrix: List[List[int]], 
+        key: int, 
+        condition: Callable[[List[int]], bool] = lambda row: True
+    ) -> Tuple[List[List[int]], int]:
+    matrix.sort(key=lambda row: row[key])
+
+    relevancy_column = len(matrix[0])
+    row_number = 0
+    prev_partition = -1
+
+    for i in range(len(matrix)):
+        matrix[i].append(0)
+        partition = matrix[i][key]
+
+        if partition != prev_partition:
+            row_number = 0
+        prev_partition = partition
+
+        if condition(matrix[i]):
+            row_number += 1
+        matrix[i][-1] = row_number
+    return matrix, relevancy_column
+
+def filter_and_mark(matrix: List[List[int]], comparison_func: Callable[[List[int]], bool]) -> Tuple[List[List[int]], int]:
+    relevancy_column = len(matrix[0])
+    for row in matrix:
+        row.append(1 if comparison_func(row) else 0)
+    return matrix, relevancy_column
