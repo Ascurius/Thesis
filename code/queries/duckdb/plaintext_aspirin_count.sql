c56e92598b7dd5922c926eaec4602db18c0d5770
Ascurius <coding@ascurius.com>
2024-06-03 20:03:06 +0200

    removed double union operator

--- code/queries/duckdb/plaintext_aspirin_count.sql
+++ code/queries/duckdb/plaintext_aspirin_count.sql
@@ -2,11 +2,7 @@ WITH union_x AS (
     (SELECT * FROM table1)
     UNION ALL
     (SELECT * FROM table2)
-), union_y AS (
-    (SELECT * FROM table1)
-    UNION ALL
-    (SELECT * FROM table2)
 )
 select COUNT(DISTINCT a.field0)
-FROM union_x a JOIN union_y b on a.field1 == b.field1
+FROM union_x a JOIN union_x b on a.field1 == b.field1
 WHERE a.field8 == 414 AND b.field4 == 0 AND a.field2 <= b.field2;
