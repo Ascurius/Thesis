a4b50d9debb4be8bb0185b6cf8b92be648495548
Ascurius <coding@ascurius.com>
2024-06-05 14:36:53 +0200

    repacked installing depencies into seperate script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install python3.10-venv
+sudo apt install python3-pip
+sudo apt install automake build-essential clang cmake git libboost-dev libboost-thread-dev libgmp-dev libntl-dev libsodium-dev libssl-dev libtool python3 libboost-filesystem-dev libboost-iostreams-dev
--- setup.sh
+++ setup.sh
@@ -5,16 +5,13 @@ mp_spdz_version="0.3.8"
 
 echo "Creating virtual environment..."
 
-sudo apt install python3.10-venv
-sudo apt install python3-pip
-
 python3 -m venv env >/dev/null
 source "env/bin/activate"
 echo "Installing dependencies..."
 pip install -r requirements.txt >/dev/null
 
 echo "Setup MP-SPDZ..."
-git clone https://github.com/data61/MP-SPDZ.git >/dev/null
+git clone git@github.com:Ascurius/MP-SPDZ.git >/dev/null
 wget https://github.com/data61/MP-SPDZ/releases/download/v0.3.8/mp-spdz-$mp_spdz_version.tar.xz >/dev/null
 tar -xf mp-spdz-$mp_spdz_version.tar.xz >/dev/null
 mv "mp-spdz-$mp_spdz_version/bin/Linux-a*" "MP-SPDZ/bin/"
@@ -25,5 +22,4 @@ python3 "$(pwd)/MP-SPDZ/setup.py" "install" >/dev/null
 cd MP-SPDZ
 ./Scripts/setup-ssl.sh
 ./Scripts/tldr.sh
-sudo apt install automake build-essential clang cmake git libboost-dev libboost-thread-dev libgmp-dev libntl-dev libsodium-dev libssl-dev libtool python3 libboost-filesystem-dev libboost-iostreams-dev
 make replicated-bin-party.x
