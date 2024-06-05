697b6d0b88d2792f004070509262d0c1502df9d0
Ascurius <coding@ascurius.com>
2024-06-05 15:01:17 +0200

    fixed errors in setup script

--- setup.sh
+++ setup.sh
@@ -3,23 +3,30 @@
 mp_spdz_version="0.3.8"
 
 
-echo "Creating virtual environment..."
+if [[ ! -d "./env" ]]; then
+    echo "Creating virtual environment..."
+    python3 -m venv env >/dev/null
+fi
 
-python3 -m venv env >/dev/null
 source "env/bin/activate"
 echo "Installing dependencies..."
 pip install -r requirements.txt >/dev/null
 
 echo "Setup MP-SPDZ..."
+
+if [[ -d "./MP-SPDZ" ]]; then
+    rm -rf "./MP-SPDZ"
+fi
+
 git clone git@github.com:Ascurius/MP-SPDZ.git >/dev/null
 wget https://github.com/data61/MP-SPDZ/releases/download/v0.3.8/mp-spdz-$mp_spdz_version.tar.xz >/dev/null
 tar -xf mp-spdz-$mp_spdz_version.tar.xz >/dev/null
-mv "mp-spdz-$mp_spdz_version/bin/Linux-a*" "MP-SPDZ/bin/"
+mv "mp-spdz-$mp_spdz_version/bin/Linux-a"/* "MP-SPDZ/bin/"
 rm -rf mp-spdz-$mp_spdz_version.tar.xz
 rm -rf mp-spdz-$mp_spdz_version
-python3 "$(pwd)/MP-SPDZ/setup.py" "install" >/dev/null
 
 cd MP-SPDZ
+python3 "./setup.py" "install" >/dev/null
 ./Scripts/setup-ssl.sh
 ./Scripts/tldr.sh
 make replicated-bin-party.x
