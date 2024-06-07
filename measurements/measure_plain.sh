d979241e2a863d78618ebbad3594f383c2121e2a
Ascurius <coding@ascurius.com>
2024-06-08 00:28:16 +0200

    fixed script for measuring the plain queries

--- measurements/measure_plain.sh
+++ measurements/measure_plain.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+path=$(pwd)
+query=$1
+query_path="$path/code/queries/secure/$query.py"
+
 if [ $# -eq 0 ]; then
     echo "No arguments provided."
     exit 1
 fi
 
-path=$(pwd)
-if [ ! -d "$path/MP-SPDZ" ]; then
-    echo "The 'MP-SPDZ' directory does not exist. Please make sure you are in the correct directory."
+if [ ! -f "$query_path" ]; then
+    echo "Query file '$query_path' could not be found!"
     exit 1
 fi
 
-echo "Running performance test for plain queries"
+num_tests=5
+extension="_plain.txt"
+out_file="$path/measurements/results/$query$extension"
 
-num_tests=$1
-query="comorbidity"
-out_file="$path/results/measurements/comorbidity_plain.txt"
+echo "Running performance test for plain queries"
 
 if [ -f "$out_file" ]; then
     rm "$out_file"
 fi
 
 rows=(1000 2000 4000 6000 8000 10000 20000 40000 60000 80000 100000 200000 400000 600000 800000 1000000)
-
 for max_rows in "${rows[@]}"; do
     echo "Performance test for $max_rows rows"
     total_execution_time=0.0
@@ -30,7 +32,7 @@ for max_rows in "${rows[@]}"; do
         populate_script="$path/code/populate.py"
         python3 "$populate_script" "secure" "$max_rows"
 
-        query_script="$path/code/queries/plain/${query}.py"
+        query_script="$path/code/queries/plain/$query.py"
         output=$(python3 "$query_script" "$max_rows")
         execution_time=$(echo "$output" | awk -F': ' '{print $2}')
         total_execution_time=$(echo "$total_execution_time + $execution_time" | bc)
