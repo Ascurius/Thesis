9cc40b4af570fe5a111336dc2c581dd784eb2ca6
Ascurius <coding@ascurius.com>
2024-06-08 22:34:57 +0200

    added additional information to measurement script

--- measurements/measure_plain.sh
+++ measurements/measure_plain.sh
@@ -16,7 +16,7 @@ if [ ! -f "$query_path" ]; then
     exit 1
 fi
 
-echo "Running performance test for plain queries"
+echo "Running performance test for plain query: $query"
 
 rows=(1000 2000 4000 6000 8000 10000 20000 40000 60000 80000 100000 200000 400000 600000 800000 1000000)
 for max_rows in "${rows[@]}"; do
@@ -24,6 +24,7 @@ for max_rows in "${rows[@]}"; do
     total_execution_time=0.0
     for ((i=1; i<=$num_tests; i++)); do
         populate_script="$path/code/populate.py"
+        echo "Generating test data..."
         python3 "$populate_script" "secure" "$max_rows"
 
         output=$(python3 "$query_path" "$max_rows")
@@ -34,4 +35,6 @@ for max_rows in "${rows[@]}"; do
     average_execution_time=$(echo "scale=6; ($total_execution_time / $num_tests)" | bc)
 
     echo "$max_rows, $average_execution_time" >> $out_file
+
+    echo "Done"
 done
