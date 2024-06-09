86b198e40fc953ff93c41e18432e355b2e717b0d
Ascurius <coding@ascurius.com>
2024-06-09 19:31:50 +0200

    allows to start experiment at specifc max_row passed as argument

--- measurements/measure_plain.sh
+++ measurements/measure_plain.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "No arguments provided. Make sure you specify the name of the query to be measured."
+if [ $# -lt 1 ]; then
+    echo "Usage: script.sh <query> [start_max_rows]"
     exit 1
 fi
 
 path=$(pwd)
 query=$1
+start_max_rows=$2
 query_path="$path/code/queries/plain/$query.py"
 num_tests=5
 out_file="$path/measurements/results/${query}_plain.txt"
@@ -19,17 +20,40 @@ fi
 echo "Running performance test for plain query: $query"
 
 rows=(1000 2000 4000 6000 8000 10000 20000 40000 60000 80000 100000 200000 400000 600000 800000 1000000)
-for max_rows in "${rows[@]}"; do
+
+# Set default start_max_rows if not specified
+if [ -z "$start_max_rows" ]; then
+    start_max_rows=${rows[0]}
+fi
+
+# Find the starting index
+start_index=-1
+for i in "${!rows[@]}"; do
+    if [ "${rows[$i]}" -eq "$start_max_rows" ]; then
+        start_index=$i
+        break
+    fi
+done
+
+# Check if the specified start_max_rows is in the list
+if [ $start_index -eq -1 ]; then
+    echo "Specified start_max_rows '$start_max_rows' is not in the predefined list of rows."
+    echo "Possible values are: ${rows[*]}"
+    exit 1
+fi
+
+echo "Generating test data..."
+populate_script="$path/code/populate.py"
+python3 "$populate_script" "secure" "1000000"
+
+for ((i=start_index; i<${#rows[@]}; i++)); do
+    max_rows=${rows[i]}
     echo "Performance test for $max_rows rows"
 
-    populate_script="$path/code/populate.py"
-    echo "Generating test data..."
-    python3 "$populate_script" "secure" "$max_rows"
-
     total_execution_time=0.0
-    for ((i=1; i<=$num_tests; i++)); do
+    for ((j=1; j<=$num_tests; j++)); do
         output=$(python3 "$query_path" "$max_rows")
-        execution_time=$(echo "$output" | awk -F': ' '{print $2}')
+        execution_time=$(echo "$output")
         total_execution_time=$(echo "$total_execution_time + $execution_time" | bc)
     done
 
