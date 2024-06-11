1fb4ec64b32a551f50b9ca14f7e673d4318653d9
Ascurius <coding@ascurius.com>
2024-06-11 17:18:52 +0200

    copied comorbidity into main

--- code/main.py
+++ code/main.py
@@ -1,71 +1,33 @@
+from Compiler.library import start_timer, stop_timer
+from typing import Callable, List
 
-
-def print_matrix(matrix):
-    for i in range(matrix.shape[0]):
-        print_ln("%s", matrix[i].reveal())
-
-def print_matches(matrix, match_key):
-    for i in range(matrix.shape[0]):
-        print_ln_if(matrix[i][match_key].reveal(), "%s", matrix[i].reveal())
-
-def get_matrix_dimensions(filename):
-    num_rows = 0
-    num_cols = None
-
-    with open(filename, 'r') as file:
-        for line in file:
-            # Increment row count for each line
-            num_rows += 1
-
-            # Split the line into integers
-            row_data = line.strip().split()
-
-            # Check if the number of columns is consistent
-            if num_cols is None:
-                num_cols = len(row_data)
-            elif num_cols != len(row_data):
-                raise ValueError("Inconsistent number of columns in the matrix")
-
-    return num_rows, num_cols
-
-def select_columns(matrix: sint.Matrix, keys: regint.Array) -> sint.Matrix:
-    keys.sort()
+def group_by_count(matrix: sint.Matrix, key: int) -> sint.Matrix:
+    matrix.sort((key,2))
     result = sint.Matrix(
         rows=matrix.shape[0],
-        columns=len(keys)
-    )
-    @for_range_opt(len(keys))
-    def _(i):
-        result.set_column(
-            i,
-            matrix.get_column(keys[i])
-        )
-    return result
-
-def groub_by_count(matrix: sint.Matrix, key: int):
-    matrix.sort((key,))
-    result = sint.Matrix(
-        rows=matrix.shape[0],
-        columns=matrix.shape[1]+2
+        columns=matrix.shape[1] + 2
     )
     @for_range_opt(matrix.shape[0])
     def _(i):
         result[i].assign_vector(matrix[i])
 
     count = sint(0)
-    rel = matrix.shape[1] # index of the relevancy column
-    agg = matrix.shape[1] + 1 # index of the aggregation column
     current_element = sint(0)
     @for_range_opt(matrix.shape[0]-1)
     def _(i):
-        nonlocal current_element
-        result[i][rel] = (matrix[i][key] == matrix[i+1][key]).if_else(sint(0), sint(1))
-        adder = (matrix[i][key] == current_element).if_else((count+sint(1)), sint(1))
+        adder = (matrix[i][key] == current_element).if_else(
+            (count+sint(1)), sint(1)
+        )
+        current_element.update(matrix[i][key])
         count.update(adder)
-        current_element = (matrix[i][key] != current_element).if_else(matrix[i][key], current_element)
-        result[i][agg] = count
-    result[-1][rel] = sint(1)
-    result[-1][agg] = (matrix[-1][key] == current_element).if_else((count+sint(1)), sint(1))
+        result[i][-2] = (matrix[i][key] == matrix[i+1][key]).if_else(
+            sint(0), sint(1)
+        )
+        result[i][-1] = count
+    result[-1][-2] = sint(1)
+    result[-1][-1] = (matrix[-1][key] == current_element).if_else(
+        (count+sint(1)), sint(1)
+    )
     return result
 
 def order_by(matrix: sint.Matrix, order_key: int, relevance_key: int, reverse: bool = False):
@@ -76,8 +38,8 @@ def order_by(matrix: sint.Matrix, order_key: int, relevance_key: int, reverse: b
     @for_range_opt(matrix.shape[0])
     def _(i):
         result[i] = matrix[i]
-        result[i][matrix.shape[1]] = matrix[i][order_key] * matrix[i][relevance_key]
-    result.sort((matrix.shape[1],))
+        result[i][-1] = matrix[i][order_key] * matrix[i][relevance_key]
+    result.sort((-1,))
     if reverse:
         swap = result.same_shape()
         @for_range_opt(result.shape[0] // 2)
@@ -97,17 +59,18 @@ def limit(matrix: sint.Matrix, maximum: int) -> sint.Matrix:
         result[i].assign_vector(matrix[i])
     return result
 
-
-p0_row, p0_col = get_matrix_dimensions("Player-Data/Input-P0-0")
-
-a = sint.Matrix(800000, p0_col)
+max_rows = 250
+a = sint.Matrix(max_rows, 13)
 a.input_from(0)
 
-columns = Array(1, regint)
-columns[0] = 1
+start_timer(100)
+g = group_by_count(a, 1)
+stop_timer(100)
 
-s = select_columns(a, columns)
-g = groub_by_count(s, 0)
+start_timer(200)
 o = order_by(g, order_key=-1, relevance_key=-2, reverse=True)
+stop_timer(200)
 
-l = limit(o, 5)
+start_timer(300)
+l = limit(o, 10)
+stop_timer(300)
