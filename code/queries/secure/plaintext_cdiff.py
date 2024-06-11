5e4154efdb451cb90145841359c3f3cea1916286
Ascurius <coding@ascurius.com>
2024-06-11 13:59:30 +0200

    added secure plaintext_cdiff query

---
+++ code/queries/secure/plaintext_cdiff.py
@@ -0,0 +1,155 @@
+from typing import Callable, List, Tuple
+
+def where(matrix: sint.Matrix, key: int, value: int) -> sint.Matrix:
+    result = sint.Matrix(
+        rows=matrix.shape[0],
+        columns=matrix.shape[1] + 1
+    )
+    @for_range_opt(matrix.shape[0])
+    def _(i):
+        result[i].assign_vector(matrix[i])
+        result[i][-1] = (matrix[i][key] == value).if_else(1,0)
+    return result
+
+def join_nested_loop(
+        left: sint.Matrix, 
+        right: sint.Matrix, 
+        left_key: int, 
+        right_key: int,
+        condition: Callable[[sint.Array, sint.Array], bool] = lambda left, right: True
+    ) -> sint.Matrix:
+    result = sint.Matrix(
+        rows=left.shape[0] * right.shape[0],
+        columns=left.shape[1] + right.shape[1] + 1
+    )
+    current_idx = regint(0)
+    @for_range_opt(left.shape[0])
+    def _(left_row):
+        @for_range_opt(right.shape[0])
+        def _(right_row):
+            new_row = sint.Array(result.shape[1])
+            @for_range_opt(left.shape[1])
+            def _(left_col):
+                new_row[left_col] = left[left_row][left_col]
+            @for_range_opt(right.shape[1])
+            def _(right_col):
+                new_row[left.shape[1]+right_col] = right[right_row][right_col]
+            result[current_idx].assign(new_row)
+            result[current_idx][-1] = (
+                (left[left_row][left_key] == right[right_row][right_key]) &
+                condition(left[left_row], right[right_row])
+            ).if_else(sint(1),sint(0))
+            current_idx.update(current_idx + regint(1))
+    return result
+
+def row_number_over_partition_by(
+        matrix: sint.Matrix, 
+        key: int, 
+        condition: Callable[[sint.Array], bool] = lambda row: True
+    ) -> sint.Matrix:
+    matrix.sort((key,))
+    result = sint.Matrix(
+        rows=matrix.shape[0],
+        columns=matrix.shape[1] + 1
+    )
+    @for_range_opt(matrix.shape[0])
+    def _(i):
+        result[i].assign_vector(matrix[i])
+
+    count = sint(0)
+    current_element = sint(0)
+    @for_range_opt(matrix.shape[0]-1)
+    def _(i):
+        new_count = (matrix[i][key] != current_element).if_else(0,count)
+        count.update(new_count)
+
+        current_element.update(matrix[i][key])
+
+        adder = (condition(matrix[i])).if_else((count+1), count)
+        count.update(adder)
+        result[i][-1] = count
+    return result
+
+def select_distinct(
+        matrix: sint.Matrix, 
+        key: int, 
+        condition: Callable[[sint.Array], bool] = lambda row: True
+    ) -> sint.Matrix:
+    matrix.sort((key,))
+    result = sint.Matrix(
+        rows=matrix.shape[0],
+        columns=matrix.shape[1] + 1
+    )
+
+    prev_value = sint(-1)
+    @for_range_opt(matrix.shape[0])
+    def _(i):
+        result[i].assign_vector(matrix[i])
+        dbit = (
+            (matrix[i][key] != prev_value) &
+            condition(matrix[i])
+        ).if_else(sint(1),sint(0))
+        result[i][-1] = dbit
+        new_value = (dbit == 1).if_else(matrix[i][key], prev_value)
+        prev_value.update(new_value)
+    return result
+
+def union_all(left, right):
+    result = sint.Matrix(
+        rows=left.shape[0] + right.shape[0],
+        columns=left.shape[1]
+    )
+    @for_range_opt(left.shape[0])
+    def _(i):
+        result[i].assign_vector(left[i])
+    @for_range_opt(right.shape[0])
+    def _(j):
+        result[left.shape[0] + j].assign_vector(right[j])
+    return result
+
+max_rows = 50
+a = sint.Matrix(max_rows, 13)
+a.input_from(0)
+b = sint.Matrix(max_rows, 13)
+b.input_from(1)
+
+union = union_all(a, b)
+
+w = where(union, 8, 8)
+w.sort((1,2))
+
+diags = row_number_over_partition_by(w, 1, condition=lambda row: row[13] == 1)
+
+def join_condition(left, right):
+    return (
+        (abs(left[2] - right[2]) >= 15) &
+        (abs(left[2] - right[2]) <= 56) &
+        (left[14]+1 == right[14])
+    ).if_else(1,0)
+
+join = join_nested_loop(
+    diags, diags, 1, 1,
+    condition=join_condition
+)
+
+def distinct_condition(row):
+    dbit = (
+        (row[13] == 1) & # where left
+        (row[-1] == 1) & # join
+        (row[-3] == 1) # where right
+    ).if_else(1,0)
+    return dbit
+
+selection = select_distinct(join, 1, condition=distinct_condition)
+
+matrix = selection
+c = sint(0)
+@for_range(matrix.shape[0])
+def _(i):
+    dbit = (matrix[i][-1] == 1).if_else(1,0)
+    @if_(dbit.reveal())
+    def _():
+        c.update(c+1)
+        # print_ln("%s", matrix[i].reveal())
+# print_ln("%s", c.reveal())
+
