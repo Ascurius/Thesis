7d5cba389d5d7727224538d92bad149d032f217d
Ascurius <coding@ascurius.com>
2024-06-19 11:49:13 +0200

    added script to plot secure operators

---
+++ code/plot/secure_op.py
@@ -0,0 +1,32 @@
+import csv
+import os
+from matplotlib.ticker import ScalarFormatter
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pwd = os.getcwd()
+query = "comorbidity"
+
+times = pd.read_csv(f"{pwd}/measurements/results/{query}_secure_op.txt", delimiter=",")
+if "total" in times.columns:
+    times = times.drop(columns=["total"])
+
+plt.figure(figsize=(12, 8))
+
+# Plot each operator's time against the number of rows
+for column in times.columns[1:]:
+    plt.plot(times['rows'], times[column], label=column)
+
+plt.xlabel('Number of rows')
+plt.ylabel('Time (seconds)')
+plt.title(f'Operator execution time in query: {query}')
+plt.legend()
+# plt.yscale('log')
+plt.grid(True)
+
+plt.gca().xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+plt.ticklabel_format(style='plain', axis='x')
+
+# Save the plot to a file
+output_path = f"{pwd}/measurements/plot/{query}_op_secure.png"
+plt.savefig(output_path)
