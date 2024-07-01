8ed17ddeb1fb4e3dbd5681e61502cc6db6e298c5
Ascurius <coding@ascurius.com>
2024-07-01 16:56:09 +0200

    migrated to sort-merge-join

--- code/queries/secure/plaintext_aspirin_count.py
+++ code/queries/secure/plaintext_aspirin_count.py
@@ -27,29 +27,45 @@ def select_distinct(
         prev_value.update(new_value)
     return result
 
-def join_nested_loop(
+def sort_merge_join(
         left: sint.Matrix, 
         right: sint.Matrix, 
-        left_key: int, 
-        right_key: int,
+        l_key: int, 
+        r_key: int,
         condition: Callable[[sint.Array, sint.Array], bool] = lambda left, right: True
     ) -> sint.Matrix:
+    left.sort((l_key,))
+    right.sort((r_key,))
+
     result = sint.Matrix(
         rows=left.shape[0] * right.shape[0],
-        columns=left.shape[1] + right.shape[1] + 1
+        columns=left.shape[1] + right.shape[1]
     )
-    current_idx = regint(0)
-    @for_range_opt(left.shape[0])
-    def _(left_row):
-        @for_range_opt(right.shape[0])
-        def _(right_row):
-            new_row = left[left_row].concat(right[right_row])
-            result[current_idx].assign(new_row)
-            result[current_idx][-1] = (
-                (left[left_row][left_key] == right[right_row][right_key]) &
-                condition(left[left_row], right[right_row])
-            ).if_else(sint(1),sint(0))
-            current_idx.update(current_idx + regint(1))
+    result.assign_all(0)
+
+    i = regint(0)
+    j = regint(0)
+    cnt = regint(0)
+
+    @while_do( (lambda: (i < right.shape[0]) & (j < left.shape[0])) )
+    def _():
+        j_adder = (right[i][r_key] > left[j][l_key]).if_else(1,0)
+        i_adder = (right[i][r_key] < left[j][l_key]).if_else(1,0)
+
+        result[cnt] = (
+            (right[i][r_key] == left[j][l_key]) &
+            condition(left[j], right[i])
+        ).if_else(
+            left[j].concat(right[i]), result[cnt]
+        )
+
+        j_adder = (right[i][r_key] == left[j][l_key]).if_else(1, j_adder)
+        i_adder = (right[i][r_key] == left[j][l_key]).if_else(1, i_adder)
+        cnt_adder = (right[i][r_key] == left[j][l_key]).if_else((cnt+1),cnt)
+
+        i.update(i + i_adder.reveal())
+        j.update(j + j_adder.reveal())
+        cnt.update(cnt_adder.reveal())
     return result
 
 def where(matrix: sint.Matrix, key: int, value: int) -> sint.Matrix:
@@ -119,7 +135,7 @@ def join_condition(left, right):
     ).if_else(1,0)
 
 start_timer(400)
-join = join_nested_loop(aw, bw, 1, 1, join_condition)
+join = sort_merge_join(aw, bw, 1, 1, join_condition)
 stop_timer(400)
 
 # start_timer(500)
