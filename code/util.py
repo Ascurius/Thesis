4e436e5c94b7bbd5107ba238fefd4f0d2025bb22
Ascurius <coding@ascurius.com>
2024-09-04 17:37:54 +0200

    removed unneeded code fragments

--- code/util.py
+++ code/util.py
@@ -2,25 +2,6 @@ import time
 from Compiler.library import print_ln, print_ln_if
 from Compiler.types import sint
 
-def string_to_array(string):
-    """
-    Wandelt einen String in ein Array von Zahlen um, wobei jede Zahl dem Unicode-Wert des entsprechenden Zeichens entspricht.
-    """
-    return [ord(char) for char in string]
-
-def find_substring(string, substring):
-    """
-    Sucht nach einem Substring in einem Array von Zahlen, das aus dem gegebenen String erstellt wurde.
-    """
-    string_array = string_to_array(string)
-    substring_array = string_to_array(substring)
-    
-    for i in range(len(string_array) - len(substring_array) + 1):
-        if string_array[i:i+len(substring_array)] == substring_array:
-            return i
-    
-    return -1  # Substring nicht gefunden
-
 def get_matrix_dimensions(filename):
     num_rows = 0
     num_cols = None
@@ -59,26 +40,6 @@ def refill_matrix(target, source):
     assert target.shape[0] >= source.shape[0]
     for i in range(source.shape[0]):
         target[i].assign_vector(source[i])
-
-def select_relevant(matrix: sint.Matrix, match_key: int) -> sint.Matrix:
-    count = regint(0)
-    vec = matrix.get_column(match_key)
-    for i in range(len(vec.elements())):
-        @if_(vec.elements()[i].reveal())
-        def _():
-            count.update(count+1)
-    result = sint.Matrix(
-        rows=count,
-        columns=matrix.shape[1]
-    )
-    result_row = regint(0)
-    for i in range(matrix.shape[0]):
-        dbit = (matrix[i][match_key] == 1).if_else(1,0)
-        @if_(dbit)
-        def _():
-            result[result_row] = matrix[i]
-            result_row.update(result_row+1)
-    return result
 
 def measure_time(func):
     def wrapper(*args, **kwargs):
